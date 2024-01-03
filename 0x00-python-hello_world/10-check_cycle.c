f52e48c1e734c6f5eb0b180f472587e55d1ed6ba
Paballo Mogane <122962125+Pablo-bit7@users.noreply.github.com>
2024-01-03 05:34:38 +0200

    Betty

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,18 +7,15 @@
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow = list, *fast = list;
+	listint_t *slow = list, *fast = list;
 
-    while (slow && fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
 
-        /* If there is a cycle, the fast and slow pointers will meet */
-        if (slow == fast)
-            return (1);
-    }
-
-    /* If the loop terminates, there is no cycle */
-    return (0);
+	return (0);
 }
