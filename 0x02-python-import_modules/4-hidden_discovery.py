faae7f12e3d16d37d8916f9e1f2bb7f30d04c687
Paballo Mogane <122962125+Pablo-bit7@users.noreply.github.com>
2024-01-05 05:30:15 +0200

    edits

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,5 @@ if __name__ == "__main__":
     from hidden_4 import *
     allfunctions = dir()
     for i in range(0, len(allfunctions)):
-        print("{:s}".format(allfunctions[i]))
+        if allfunctions[i][:2] != "__":
+            print("{:s}".format(allfunctions[i]))
