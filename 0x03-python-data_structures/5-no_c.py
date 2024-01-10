9995f8a43f6866ec9e4d27ba7739919c25cf5707
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-01-10 04:54:38 +0200

    Modified 5-no_c.py again

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_str = my_string.translate({ord("c"): None})
-    sec_str = new_str.translate({ord("C"): None})
-    return sec_str
+    if my_string[:]:
+        new_str = my_string.translate({ord("c"): None})
+        sec_str = new_str.translate({ord("C"): None})
+        return sec_str
+    return my_string
 
