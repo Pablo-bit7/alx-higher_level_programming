d20d6099c27b0222179ef666ac2d4893d5a93fd1
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-01-11 03:23:04 +0200

    9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """
+    Returns a new dictionary with all values multiplied by 2.
+
+    Args:
+    - a_dictionary (dict): Input dictionary with integer values.
+
+    Returns:
+    - dict: New dictionary with values multiplied by 2.
+    """
+    new_dict = {key: value * 2 for key, value in a_dictionary.items()}
+    return new_dict
+
