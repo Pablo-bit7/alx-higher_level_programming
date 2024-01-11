ba882f1f00c8bfcb00a2f7424eea6ea91a2b5be0
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-01-11 03:06:39 +0200

    5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+
+    Args:
+    - a_dictionary (dict): Input dictionary.
+
+    Returns:
+    - int: Number of keys in the dictionary.
+    """
+    num_keys = len(a_dictionary.keys())
+    return num_keys
+
