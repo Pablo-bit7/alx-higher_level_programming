42e803fca2dd3310b4c05557a19257acda8b9e8a
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-01-11 03:33:23 +0200

    11-multiply_list_map.py

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+    """
+    Returns a new list with all values multiplied by a specified number using map.
+
+    Args:
+    - my_list (list): Input list of integers.
+    - number (int): Number to multiply each value in the list.
+
+    Returns:
+    - list: New list with each value multiplied by the specified number.
+    """
+    return list(map(lambda x: x * number, my_list))
+
