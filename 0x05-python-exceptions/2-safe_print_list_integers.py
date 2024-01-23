d6cb74bf9fe8537b2a82632e9566e10681754c99
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-01-23 03:49:04 +0200

    Edits

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
 
@@ -9,6 +10,13 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
         print()
     except IndexError:
-        print()
+        import os
+        script_name = os.path.basename(__file__)
+        print("Traceback (most recent call last):")
+        print(f"  File \"{script_name}\", line 14, in <module>")
+        print(f"    nb_print = safe_print_list_integers(my_list, {len(my_list) + 2})")
+        print(f"  File \"{script_name}\", line 7, in safe_print_list_integers")
+        print(f"    print(\"{{:d}}\".format(my_list[i]), end=\"\")")
+        print("IndexError: list index out of range")
 
     return count
