aa3976b3f69c3bc1f80a4611388f4a5d7b3fc84a
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-02-06 03:28:02 +0200

    1-my_list.py, tests/1-my_list.txt

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines an inherited list class MyList."""
+
+class MyList(list):
+    """Implements sorted printing for the built-in list class.
+
+    Public instance method:
+    - def print_sorted(self): Prints the list, but sorted in ascending order.
+    """
+    
+    def print_sorted(self):
+        """Prints the list in ascending sorted order."""
+        print(sorted(self))
