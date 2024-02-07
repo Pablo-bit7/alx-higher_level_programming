485f22afa98ea33997f1f224c74e1f7ccbb37c38
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-02-07 05:12:05 +0200

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Defines a function that returns an object (Python data structure)
+represented by a JSON string.
+"""
+
+
+import json
+
+def from_json_string(my_str):
+    """
+    Return an object (Python data structure) represented by a JSON string.
+
+    Parameters:
+    - my_str (str): The JSON string.
+
+    Returns:
+    - object: The Python data structure represented by the JSON string.
+    """
+    return json.loads(my_str)
