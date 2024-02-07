cdc3cdccca7eae8fc71e8a6b16fda47772e1e267
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-02-07 04:44:32 +0200

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 Defines a function that reads a text file (UTF8) and prints it to stdout.
 """
 
+
 def read_file(filename=""):
     """
     Read a text file (UTF8) and print its content to stdout.
