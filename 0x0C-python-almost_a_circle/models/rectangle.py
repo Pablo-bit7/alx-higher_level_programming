41902f239dabda042907c0cc6d143b5ddcffb78f
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-02-13 05:13:33 +0200

    Even more fixes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """Defines a rectangle."""
 
-    __nb_objects = 0
+    __nb_objects = 1
 
     def __init__(self, width=0, height=0, x=0, y=0, id=None):
         """Initializes a new rectangle.
