bc6663ce6671e3d8fe73cdb24360a1db775add2b
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-02-13 05:47:25 +0200

    Added def update(self, *args, **kwargs):

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,6 +17,35 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Gets the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If the size is not an integer.
+            ValueError: If the size is less than or equal to 0.
+        """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Assigns attributes to the square: id, size, x, y."""
+        if args:
+            attr_list = ["id", "size", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attr_list[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     def __str__(self):
         """Returns a string representation of the square."""
         return "[Square] ({}) {}/{} - {}".format(
