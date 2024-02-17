1c2682abe481f1d3ee5b07a22c4b0d2a83c14868
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-02-17 03:32:21 +0200

    Hmm

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,5 +23,30 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ Returns the JSON string representation of list_dictionaries. """
+        """ Returns the JSON string representation of a list of dictionaries.
+
+        Parameters:
+        - list_dictionaries (list): A list of dictionaries.
+
+        Returns:
+        - str: The JSON string representation of the list of dictionaries.
+        """
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Writes the JSON string representation of list_objs to a file.
+
+        Parameters:
+        - list_objs (list): A list of instances that inherit from Base.
+
+        Returns:
+        - None
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, mode="w", encoding="utf-8") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                file.write(cls.to_json_string(list_dicts))
