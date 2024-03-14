69c890e8186c646bd430f136cfaa3367e7511ad7
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-03-14 04:18:46 +0200

    4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- Description: Creates the table first_table if it doesn't already exist
+
+/* Create the table first_table if it doesn't exist */
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
