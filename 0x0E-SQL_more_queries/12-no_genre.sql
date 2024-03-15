eb16777e0ed66a96e838a385f56f1524bb267aae
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-03-15 05:59:05 +0200

    11-genre_id_all_shows.sql & 15-comedy_only.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,9 @@
+-- Description: List all shows without a linked genre
+
+-- Select tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC;
+
