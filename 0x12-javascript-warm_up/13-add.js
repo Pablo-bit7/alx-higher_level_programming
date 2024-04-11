d5e764e4f2672b53c1a30f13b363e75109c0b99d
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-04-11 05:47:56 +0200

    13-add.js

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/**
+ * Function to add two integers.
+ * @param {number} a - The first integer.
+ * @param {number} b - The second integer.
+ * @returns {number} - The sum of the two integers.
+ */
+exports.add = function (a, b) {
+  return a + b;
+};
+
