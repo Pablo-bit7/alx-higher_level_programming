e232247df1fbe775a5dfab8977795c9246c272f1
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-06-29 01:28:36 +0200

    model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This script defines the State class and links it to the states table
+in the MySQL database using SQLAlchemy.
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create an instance of the Base class
+Base = declarative_base()
+
+class State(Base):
+    """
+    State class that links to the MySQL table 'states'.
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    import sys
+    # Create an engine and connect to the MySQL database
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    # Create all tables in engine (equivalent to "Create Table" in raw SQL)
+    Base.metadata.create_all(engine)
