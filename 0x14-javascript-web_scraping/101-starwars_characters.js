b4e9b295463f95ab6ac98e3a5ef52625fb8898fe
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-07-31 05:45:04 +0200

    101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+if (!movieId) {
+  console.error('Usage: ./101-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+  if (response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const characterUrls = data.characters;
+
+    characterUrls.forEach((characterUrl, index) => {
+      request(characterUrl, (err, res, characterBody) => {
+        if (err) {
+          console.error('Error:', err);
+          return;
+        }
+        if (res.statusCode === 200) {
+          const characterData = JSON.parse(characterBody);
+          console.log(characterData.name);
+        }
+      });
+    });
+  } else {
+    console.error(`Error: ${response.statusCode}`);
+  }
+});
