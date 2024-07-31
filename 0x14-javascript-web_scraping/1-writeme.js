3b0fe7416b359008f34321ebe302b3ef67b01a37
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-07-31 05:02:39 +0200

    1-wrModified witeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 
+const { argv } = process;
 const fs = require('fs');
-const filePath = process.argv[2];
-const content = process.argv[3];
 
-if (!filePath || !content) {
+if ((process.argv.length < 4) {
   console.error('Usage: ./1-writeme.js <file path> <string to write>');
   process.exit(1);
 }
 
-fs.writeFile(filePath, content, 'utf8', err => {
+fs.writeFile(argv[2], argv[3], err => {
   if (err) {
     console.error(err);
     process.exit(1);
